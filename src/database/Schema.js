a42ec9b8fd23658ac14946893baa6fa2be545566
Esteban <96262806+Castelblanco@users.noreply.github.com>
2022-08-01 15:47:48 -0500

    Update Schema.js

--- src/database/Schema.js
+++ src/database/Schema.js
@@ -3,10 +3,10 @@ import { Schema, model } from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 
 const schema = new Schema({
-          img: {type: Buffer, required: true},
+          img: {type: String, required: true},
           wordKey: {type: String, required: true},
           type: {type: String, required: true}
       });
 
 schema.plugin(mongoosePaginate);
-export default model("image", schema);
+export default model("images", schema);
